f688e96d686e61d9c1011ed034c1d0d9836f193a
Dipanshu Singh <69574915+Dipanshu8@users.noreply.github.com>
2020-08-15 22:17:27 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Account.h"
+
+int main()
+{
+  int n;
+  cout<<"Enter 1 to deposit , 2 to withdraw , 3 to get balance "; 
+  cin>>c;
+  if(c==1){
+  Account a1; //Account object//
+  int d
+  cout<<"Enter amount to deposit: ";
+  cin>>d
+  a1.deposit(a);
