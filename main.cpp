68dabb2ae9bb2e7ee93ba25781412c29821119cb
Dipanshu Singh <69574915+Dipanshu8@users.noreply.github.com>
2020-08-15 23:25:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-#include<iostream>
-#include<fstream>
+#include<stdio.h> 
+#include<iostream> 
+#include<fstream> 
+#include<string.h>
 #include<cstdlib>
-using std::cout;
-using std::cin;
-using std::endl;
-using std::fstream;
-using std::ofstream;
-using std::ifstream;
-using std::ios;
+using namespace std;
 class account_query
 {
 private:
