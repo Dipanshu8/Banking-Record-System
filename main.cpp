0052c4e7ba7c1a7c5792d3d6ba0a76a6c43b5cd4
Dipanshu Singh <69574915+Dipanshu8@users.noreply.github.com>
2020-08-16 16:04:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -43,15 +43,15 @@ void account_query::show_data()
 void account_query::write_rec()
 {
     ofstream outfile;
-    outfile.open("record.bank", ios::binary|ios::app);
+    outfile.open("record.txt", ios::app);
     read_data();
-    outfile.write(reinterpret_cast<char *>(this), sizeof(*this));
+    outfile.write();
     outfile.close();
 }
 void account_query::read_rec()
 {
     ifstream infile;
-    infile.open("record.bank", ios::binary);
+    infile.open("record.txt");
     if(!infile)
     {
         cout<<"Error in Opening! File Not Found!!"<<endl;
@@ -60,7 +60,7 @@ void account_query::read_rec()
     cout<<"\n****Data from file****"<<endl;
     while(!infile.eof())
     {
-        if(infile.read(reinterpret_cast<char*>(this), sizeof(*this))>0)
+        if(infile.read()>0)
         {
             show_data();
         }
@@ -71,76 +71,76 @@ void account_query::search_rec()
 {
     int n;
     ifstream infile;
-    infile.open("record.bank", ios::binary);
+    infile.open("record.txt");
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     infile.seekg(0,ios::end);
-    int count = infile.tellg()/sizeof(*this);
+    int count = infile.tellg();
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to Search: ";
     cin>>n;
-    infile.seekg((n-1)*sizeof(*this));
-    infile.read(reinterpret_cast<char*>(this), sizeof(*this));
+    infile.seekg((n-1));
+    infile.read();
     show_data();
 }
 void account_query::edit_rec()
 {
     int n;
     fstream iofile;
-    iofile.open("record.bank", ios::in|ios::binary);
+    iofile.open("record.txt", ios::in);
     if(!iofile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     iofile.seekg(0, ios::end);
-    int count = iofile.tellg()/sizeof(*this);
+    int count = iofile.tellg();
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to edit: ";
     cin>>n;
-    iofile.seekg((n-1)*sizeof(*this));
-    iofile.read(reinterpret_cast<char*>(this), sizeof(*this));
+    iofile.seekg((n-1));
+    iofile.read();
     cout<<"Record "<<n<<" has following data"<<endl;
     show_data();
     iofile.close();
-    iofile.open("record.bank", ios::out|ios::in|ios::binary);
-    iofile.seekp((n-1)*sizeof(*this));
+    iofile.open("record.txt", ios::out|ios::in|);
+    iofile.seekp((n-1));
     cout<<"\nEnter data to Modify "<<endl;
     read_data();
-    iofile.write(reinterpret_cast<char*>(this), sizeof(*this));
+    iofile.write();
 }
 void account_query::delete_rec()
 {
     int n;
     ifstream infile;
-    infile.open("record.bank", ios::binary);
+    infile.open("record.txt");
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     infile.seekg(0,ios::end);
-    int count = infile.tellg()/sizeof(*this);
+    int count = infile.tellg();
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to Delete: ";
     cin>>n;
     fstream tmpfile;
-    tmpfile.open("tmpfile.bank", ios::out|ios::binary);
+    tmpfile.open("tmpfile.txt", ios::out);
     infile.seekg(0);
     for(int i=0; i<count; i++)
     {
-        infile.read(reinterpret_cast<char*>(this),sizeof(*this));
+        infile.read();
         if(i==(n-1))
             continue;
-        tmpfile.write(reinterpret_cast<char*>(this), sizeof(*this));
+        tmpfile.write();
     }
     infile.close();
     tmpfile.close();
-    remove("record.bank");
-    rename("tmpfile.bank", "record.bank");
+    remove("record.txt");
+    rename("tmpfile.txt", "record.txt");
 }
 int main()
 {
