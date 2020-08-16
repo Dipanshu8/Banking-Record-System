8ef3d373a11b867e3ec5880e557f12a7273a80e7
Dipanshu Singh <69574915+Dipanshu8@users.noreply.github.com>
2020-08-16 16:41:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include<stdio.h> 
-#include<iostream> 
-#include<fstream> 
-#include<string.h>
+#include<iostream>
+#include<fstream>
 #include<cstdlib>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
 class account_query
 {
 private:
@@ -43,15 +47,15 @@ void account_query::show_data()
 void account_query::write_rec()
 {
     ofstream outfile;
-    outfile.open("record.txt", ios::app);
+    outfile.open("record.bank", ios::binary|ios::app);
     read_data();
-    outfile.write();
+    outfile.write(reinterpret_cast<char *>(this), sizeof(*this));
     outfile.close();
 }
 void account_query::read_rec()
 {
     ifstream infile;
-    infile.open("record.txt");
+    infile.open("record.bank", ios::binary);
     if(!infile)
     {
         cout<<"Error in Opening! File Not Found!!"<<endl;
@@ -60,7 +64,7 @@ void account_query::read_rec()
     cout<<"\n****Data from file****"<<endl;
     while(!infile.eof())
     {
-        if(infile.read()>0)
+        if(infile.read(reinterpret_cast<char*>(this), sizeof(*this))>0)
         {
             show_data();
         }
@@ -71,76 +75,76 @@ void account_query::search_rec()
 {
     int n;
     ifstream infile;
-    infile.open("record.txt");
+    infile.open("record.bank", ios::binary);
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     infile.seekg(0,ios::end);
-    int count = infile.tellg();
+    int count = infile.tellg()/sizeof(*this);
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to Search: ";
     cin>>n;
-    infile.seekg((n-1));
-    infile.read();
+    infile.seekg((n-1)*sizeof(*this));
+    infile.read(reinterpret_cast<char*>(this), sizeof(*this));
     show_data();
 }
 void account_query::edit_rec()
 {
     int n;
     fstream iofile;
-    iofile.open("record.txt", ios::in);
+    iofile.open("record.bank", ios::in|ios::binary);
     if(!iofile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     iofile.seekg(0, ios::end);
-    int count = iofile.tellg();
+    int count = iofile.tellg()/sizeof(*this);
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to edit: ";
     cin>>n;
-    iofile.seekg((n-1));
-    iofile.read();
+    iofile.seekg((n-1)*sizeof(*this));
+    iofile.read(reinterpret_cast<char*>(this), sizeof(*this));
     cout<<"Record "<<n<<" has following data"<<endl;
     show_data();
     iofile.close();
-    iofile.open("record.txt", ios::out|ios::in|);
-    iofile.seekp((n-1));
+    iofile.open("record.bank", ios::out|ios::in|ios::binary);
+    iofile.seekp((n-1)*sizeof(*this));
     cout<<"\nEnter data to Modify "<<endl;
     read_data();
-    iofile.write();
+    iofile.write(reinterpret_cast<char*>(this), sizeof(*this));
 }
 void account_query::delete_rec()
 {
     int n;
     ifstream infile;
-    infile.open("record.txt");
+    infile.open("record.bank", ios::binary);
     if(!infile)
     {
         cout<<"\nError in opening! File Not Found!!"<<endl;
         return;
     }
     infile.seekg(0,ios::end);
-    int count = infile.tellg();
+    int count = infile.tellg()/sizeof(*this);
     cout<<"\n There are "<<count<<" record in the file";
     cout<<"\n Enter Record Number to Delete: ";
     cin>>n;
     fstream tmpfile;
-    tmpfile.open("tmpfile.txt", ios::out);
+    tmpfile.open("tmpfile.bank", ios::out|ios::binary);
     infile.seekg(0);
     for(int i=0; i<count; i++)
     {
-        infile.read();
+        infile.read(reinterpret_cast<char*>(this),sizeof(*this));
         if(i==(n-1))
             continue;
-        tmpfile.write();
+        tmpfile.write(reinterpret_cast<char*>(this), sizeof(*this));
     }
     infile.close();
     tmpfile.close();
-    remove("record.txt");
-    rename("tmpfile.txt", "record.txt");
+    remove("record.bank");
+    rename("tmpfile.bank", "record.bank");
 }
 int main()
 {
